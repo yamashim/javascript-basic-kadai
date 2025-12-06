7b29f5873fb37fa9f6ee2d0d6091e8cab0d79c28
obungu <95140069+yamashim@users.noreply.github.com>
2025-12-06 23:06:48 +0900

    mdn課題ファイルの提出

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,15 @@
+//今日の日付をコンソールへ出力する関数
+const printToday = () => {
+    //Date関数で現時刻（この時点ではミで取得
+    const dateNow = new Date();
+
+    //toLocaleString関数でyyyy/mm/dd hh/mm/ss表示に変換
+    //console.log(dateNow.toLocaleString());
+
+    //年月日表示でコンソールへ出力する
+    console.log(dateNow.getFullYear() + "年" + dateNow.getMonth() + "月" + dateNow.getDate() + "日");
+
+}
+
+//関数を呼び出す
+printToday();
